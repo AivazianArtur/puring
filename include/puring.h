e8398c3245cf5621e276391bd66b2df8b057548a
pullya <52126850+AivazianArtur@users.noreply.github.com>
2026-01-15 01:02:51 +0300

    Integrating PythonC API

--- include/puring.h
+++ include/puring.h
@@ -1,5 +1,20 @@
 #pragma once  // Alternative of `include guards (IFNDEF)`
 #include "liburing.h"
 
-int puring_init(struct io_uring *ring);
-void puring_exit(struct io_uring *ring);
+
+/* ShadowRegistry to shadow PyObjects from GC*/
+typedef struct {
+    PyObject **objects;
+    unsigned int size;  // Should match ring entries (e.g., 256)
+} RequestRegistry;
+
+
+/* Ring Wrapper*/
+typedef struct {
+    PyObject_HEAD
+    struct io_uring ring;
+    RequestRegistry registry; 
+    bool initialized;
+    int fd;                 // File descriptor
+    void *buffer;           // The buffer where Kernel puts data
+} UringObject;
--- src/puring.c
+++ src/puring.c
@@ -1,10 +1,1 @@
 #include "puring.h"
-
-// Ring Wrapper (for request(TEMP: maybe for everything))
-typedef struct {
-    // PyObject_HEAD
-    struct io_uring *ring;  // Reference to the main ring
-    // PyObject *future;       // The Python asyncio.Future to wake up
-    int fd;                 // File descriptor
-    void *buffer;           // The buffer where Kernel puts data
-} UringObject;
