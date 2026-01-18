46ff4496ad8f2a8bcfed1cdd79bd36310830e4f8
pullya <52126850+AivazianArtur@users.noreply.github.com>
2026-01-18 23:58:16 +0300

    now familiar with cpython api

--- include/puring.h
+++ include/puring.h
@@ -1,20 +1,2 @@
-#pragma once  // Alternative of `include guards (IFNDEF)`
+#pragma once
 #include "liburing.h"
-
-
-/* ShadowRegistry to shadow PyObjects from GC*/
-typedef struct {
-    PyObject **objects;
-    unsigned int size;  // Should match ring entries (e.g., 256)
-} RequestRegistry;
-
-
-/* Ring Wrapper*/
-typedef struct {
-    PyObject_HEAD
-    struct io_uring ring;
-    RequestRegistry registry; 
-    bool initialized;
-    int fd;                 // File descriptor
-    void *buffer;           // The buffer where Kernel puts data
-} UringObject;
--- src/puring.c
+++ src/puring.c
@@ -1,1 +1,24 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <stdio.h>
 #include "puring.h"
+
+
+void main(int *draft_args[], char *draft_kwargs[])
+{
+    // struct io_uring_params params;
+    // puring_init((struct io_uring_params){ .draft = 25, .another_draft = 28 });
+
+
+    int ret = puring_init(&ring);
+    if (ret < 0) {
+        perror("puring_init failed");
+        return 1;
+    }
+
+    printf("io_uring initialized\n");
+
+    puring_exit(&ring);
+    return 0;
+}
